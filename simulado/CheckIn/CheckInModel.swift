0d47869d5a39aa60684bd94c5d0e674e7e763529
Kevin Souza <kevin.souza@softdesign.com.br>
2021-08-03 13:30:08 -0300

    2.0v post

---
+++ simulado/CheckIn/CheckInModel.swift
@@ -0,0 +1,13 @@
+//
+//  CheckInModel.swift
+//  simulado
+//
+//  Created by Kevin willian Jorge souza on 03/08/21.
+//
+
+import Foundation
+
+struct CheckInModel: Codable {
+    var name: String?
+    var email: String?
+}
--- simulado/CheckIn/CheckInWorker.swift
+++ simulado/CheckIn/CheckInWorker.swift
@@ -10,8 +10,21 @@ import Foundation
 class CheckInWorker {
     
     
-    func postCheckInEvent() {
+    func postCheckInEvent(body: CheckInModel, onComplete: @escaping (String) -> Void) {
+        let endPoint = "checkin"
         
+        guard let json = try? JSONEncoder().encode(body) else{
+            return }
         
+        Rest.loadAPI(json: json, header: nil, endPointPath: endPoint, HTTPMethod: .POST) { (data, error) in
+            
+            do{
+                let data = try JSONDecoder().decode(CheckInModel.self, from: data)
+                onComplete(data)
+            }
+            catch {
+                print(error)
+            }
+        }
     }
 }
